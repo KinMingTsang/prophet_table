89352aeda0e6a24e2a80276bc2ada08736d7862d
kmt_kin <63468364+KinMingTsang@users.noreply.github.com>
2023-07-07 19:59:02 +0900

    Implement pandas feature into the prophet table library

--- main.py
+++ main.py
@@ -28,18 +28,12 @@ def get_files(path,sorting=""):
 
 tables =  get_files(src2,(lambda src:  True if src.find(".PRO")!=-1 else False))
 
-df1 = prophet_table(path = src2,file = tables[0],gen_key = True)
-df2 = prophet_table(path = src1,file =  tables[1], gen_key = True)
-df1.show_detail()
-df1 = prophet_table( path = df1.get_path(), file = df1.get_filename()+df1.get_ext(), object =  df1[df1["SPCODE"]=='101000'],show_leading = True)
-# print(df1)
-df1.show_detail()
-print(type(df1))
-# dfx2 = df2.sort_records('101000',target_col="SPCODE")
-# print(dfx2)
-# print(type(dfx2))
-# result =  df1.compare(df2)
-# print(type(df1))
+df1 = prophet_table(path = src2, file = tables[0], gen_key = True)
+df2 = prophet_table(path = src1, file =  tables[1], gen_key = True)
+
+df1 = df1[df1['AGE_AT_ENTRY']<'40']
+print(df1)
+print(df1.compare(df2))
 
 # for file in tqdm(get_files(src2,(lambda src:  True if src.find(".fac")!=-1 and src.find("202212")!=-1 else False)),desc = "Table checking progress",unit = "tables"):
 # for file in tqdm(get_files(src2,(lambda src:  True if src.find(".fac")!=-1 else False)),desc = "Table checking progress",unit = "tables"):
--- prophet_table.py
+++ prophet_table.py
@@ -8,12 +8,11 @@ import types
 # def report_error(msg = ""):
 class prophet_table():
 
-    __expected_ext__ = [".R",".N",".l",".PRO",".fac"]
     __key_num__ = 0
-    __show_leading__ = False
+    show_leading = False
 
 
-    def __init__(self, object =None, path="", file="", gen_key=False, show_leading = False)->None:
+    def __init__(self, object =None, path="", file="", gen_key=False, show_leading = False, show_table = False,show_info = False)->None:
         #object: Pandas or prophet_table expected
         #path: string the input directory of the file
         #file: string file name with extension 
@@ -34,12 +33,8 @@ class prophet_table():
             self.__path__ = (lambda x : x+"\\" if x.rfind("\\") != len(x)-1 else x)(path)
             self.__file_name__ = file[0:file.find(self.__ext__)]
             self.__load_table__()
-            self.__show_leading__ = show_leading
 
-            if gen_key:
-                self.__gen_key__()
-
-        elif isinstance(object,pd.DataFrame):
+        elif isinstance(object,pd.DataFrame) :
             if not(isinstance(path,str)) or not(isinstance(file,str)):
                 raise TypeError
 
@@ -48,61 +43,136 @@ class prophet_table():
 
             if not(file in os.listdir(path)):
                 raise FileNotFoundError
-            
-            
+            if object.columns[0].find("!")==-1:
+                object  =  object.to_frame().insert(0,"!"+object.shape(1),"*")
+
             self.__load_ext__(file = file)
             self.__path__ = (lambda x : x+"\\" if x.rfind("\\") != len(x)-1 else x)(path)
             self.__file_name__ = file[0:file.find(self.__ext__)]
             self.__content__ = object
-            self.__show_leading__ = show_leading
+            self.show_leading = show_leading
+            
             if gen_key:
-                self.__gen_key__()
+                self.gen_key()
+        
+        elif isinstance(object, pd.Series):
+            
+            if not(isinstance(path,str)) or not(isinstance(file,str)):
+                raise TypeError
 
+            if  not(os.path.exists(path)):
+                raise Exception("Path not found error")
+
+            if not(file in os.listdir(path)):
+                raise FileNotFoundError
+
+            if object.dtype == bool:
+                raise Exception("Boolean Value Series Cannot be used as a table")
+
+            object  =  object.to_frame().insert(0,"!1","*")
+
+            self.__load_ext__(file = file)
+            self.__path__ = (lambda x : x+"\\" if x.rfind("\\") != len(x)-1 else x)(path)
+            self.__file_name__ = file[0:file.find(self.__ext__)]
+            self.__content__ = object
+            self.show_leading = show_leading
+            
+            if gen_key:
+                self.gen_key()
+        
+        elif isinstance(object,prophet_table):
+            self.__eq__(object)
+        
         else:
-            print("Invalid Data Type")
+            raise Exception("Invalid Data Type")
+        
+        self.index = self.__content__.index
+        self.shape =   (lambda x: x if self.show_leading else (x[0],x[1]-1))(self.__content__.shape)
+        self.columns = self.__content__.columns
+        self.show_leading = show_leading
+        self.show_table = show_table
+        self.show_info = show_info
 
-    def haeds(self)->pd.Series:
-        return self.__content__.heads()
-
-    def columns(self)->pd.Index:
-        return self.__content__.columns
+        if gen_key:
+            self.gen_key()
 
 
-    def __eq__(self,object2)->None:
-        if isinstance(object2,prophet_table):
-            self.__content__ = object2.__content__
-            self.__key_num__ = object2.__key_num__
-            self.__show_leading__ = object2.__show_leading__
-            self.__path__ = object2.__path__
-            self.__file_name__ = object2.__file_name__
-            self.__ext__ = object2.__ext__
-
-        if isinstance(object2,pd.DataFrame):
-            self.__content__ = object2.__content__
-            self.__key_num__ = object2.__key_num__
-            self.__show_leading__ = object2.__show_leading__
-            self.__path__ = object2.__path__
-            self.__file_name__ = object2.__file_name__
-            self.__ext__ = object2.__ext__
+    def head(self,row_num)->pd.Series:
+        return self.__content__.head(row_num)
 
     def __load_ext__(self, file)->None:
         #file string, input file name expected ending with extenstion
         #this function is designed to obtained the file extension 
-        for ext in self.__expected_ext__:
+        for ext in [".R",".N",".l",".PRO",".fac"]:
             if file.find(ext)!=-1:
                 self.__ext__ = ext
                 return
         print("Extension value error, not a valid extension")
         raise ValueError
 
-    def __getitem__(self, key):
+#####################################################################################Function OVERLOADING ##############################################
+
+    def __getitem__(self, key)->pd.Series:
         #key: the key index required
         #override the [] functions
-        return prophet_table( object = self.__content__[key], path = self.__path__, file = self.__file_name__+self.__ext__,show_leading= self.__show_leading__)
+        result = self.__content__[key]
         
+        if isinstance(result,pd.Series):
+            return result
+            
+        return prophet_table( object = result, path = self.__path__, file = self.__file_name__+self.__ext__, show_table = self.show_table, show_info = self.show_info, show_leading = self.show_leading)
 
+    def __str__(self):
+        ##Print function overloading
+        if self.show_info:
+            if self.show_table:
+                return ">>>>>>File source:\t {}\n>>>>>>File type:\t {}\n>>>>>>File Shape:\t {}\n>>>>>>File Settings:\t key_num = {},\t show_leading = {}\n{}".format(self.__path__+self.__file_name__+self.__ext__,                            
+                                                                                                                                                                    (lambda file_extension: (lambda file_extension: "Model Point File" if file_extension == ".PRO" else "Result file")(file_extension) if file_extension !=".fac" else "Table")(self.__ext__), 
+                                                                                                                                                                    self.shape, self.__key_num__,self.show_leading,(lambda has_leading:self.__content__ if has_leading else  self.__content__[[x for x in self.__content__.keys()[1:]]])(self.show_leading))
+            return ">>>>>>File source:\t {}\n>>>>>>File type:\t {}\n>>>>>>File Shape:\t {}\n>>>>>>File Settings:\t key_num = {},\t show_leading = {}".format(self.__path__+self.__file_name__+self.__ext__,                            
+                                                                                                                                                                    (lambda x: (lambda y: "Model Point File" if y == ".PRO" else "Result file")(x) if x !=".fac" else "Table")(self.__ext__), self.shape, self.__key_num__,self.show_leading)
+        else:
+            return ">>>>>>File Shape:\t {}\n>>>>>>File Settings:\t key_num = {},\t show_leading = {}\n{}".format(self.shape, self.__key_num__, self.show_leading, (lambda has_leading:self.__content__ if has_leading else  self.__content__[[x for x in self.__content__.keys()[1:]]])(self.show_leading))
 
+    def __lt__(self, other)->pd.Series:
+         
+        if isinstance(other,prophet_table):
+            return self.__content__<other.get_content()
+        else:
+            return self.__content__<other
 
+    def __le__(self, other)->pd.Series:
+        if isinstance(other,prophet_table):
+            return self.__content__<=other.get_content()
+        else:
+            return self.__content__<=other
+
+    def __eq__(self, other)->pd.Series:
+        if isinstance(other,prophet_table):
+            return self.__content__==other.get_content()
+        else:
+            return self.__content__==other
+    
+    def __ne__(self, other)->pd.Series:
+        if isinstance(other,prophet_table):
+            return self.__content__!=other.get_content()
+        else:
+            return self.__content__!=other
+
+    def __gt__(self, other)->pd.Series:
+        if isinstance(other,prophet_table):
+            return self.__content__>other.get_content()
+        else:
+            return self.__content__>other
+
+    def __ge__(self, other)->pd.Series:
+        if isinstance(other,prophet_table):
+            return self.__content__>=other.get_content()
+        else:
+            return self.__content__>=other
+
+#####################################################################################MUTATOR FUNCTIONS ##############################################
+     
     def __key_loc__(self, key, lookup)->int:
         #this function will return the index of the key
         #lookup: the table you want to check with. Key: The target you want to find
@@ -138,19 +208,20 @@ class prophet_table():
             self.__content__.dropna(axis=0,inplace = True)
         
         self.__find_key_num__()
-        # self.__content__.drop(self.__content__.columns[0], axis=1,inplace= True) # to drop the column with *
-  
+        # self.__content__.drop(self.columns[0], axis=1,inplace= True) # to drop the column with *
+
     def __find_key_num__(self)->int:
         #df the target dataframe, pandas dataframe is expected
         # value_compare: Boolean, True when you want to compare the value for whole table
-        if len((self.__content__).columns[0])!= 1:
-            self.__key_num__ = int(((self.__content__).columns[0])[1:]) #set this variable to the number of column that needs to be used as key
+        if len(self.__content__.columns[0])!= 1:
+            self.__key_num__ = int((self.__content__.columns[0])[1:]) #set this variable to the number of column that needs to be used as key
+            
             if self.__key_num__ >self.__content__.shape[1]:
                 self.__key_num__ = self.__content__.shape[1]
         else:
             raise Exception("Invalid Key Num")
-       
-    def __gen_key__(self, value_compare=False)->None:
+
+    def gen_key(self, value_compare=False)->None:
         # value_compare: Boolean, True when you want to compare the value for whole table
         #this function will attach the key to the  target dataframe with key attached
                     
@@ -179,6 +250,47 @@ class prophet_table():
 
         self.__content__["Key"] = cur_key
 
+#####################################################################################UTILITY FUNCTIONS ##############################################
+    def compare(self, table2)->pd.DataFrame:
+        # fac2 = table 2 you imported prophet_table expected
+        #  return the result of comparison with two columns [Lookup_Key,Result ] in pandas dataframe
+
+        if not(isinstance(table2,prophet_table)):
+            raise Exception("The file is not prophet table object")
+        self.gen_key(True)
+        
+        table2.gen_key(True)
+        
+        result=pd.DataFrame()   
+
+        try:
+            result["Lookup_Key"] = pd.concat([self.__content__["Key"], table2.get_content()["Key"]], axis=0, ignore_index=True).unique()
+        
+        except ValueError:
+            result["Lookup_Key"] = pd.Series(pd.concat([self.__content__["Key"],table2.get_content()["Key"]], axis=0, ignore_index=True).unique()).reindex()
+        
+        temp = []
+
+        for key in result["Lookup_Key"]:
+
+            check1 = self.__key_loc__(key,self.__content__["Key"])
+            check2 = self.__key_loc__(key, table2["Key"])
+            
+            if check1!=-1 and check2!=-1 :
+                temp.append("Matched in both file")
+            
+            elif check1!=-1:
+                temp.append("Matched in fac1 only")
+            
+            else:
+                temp.append("Matched in fac2 only")
+
+        result["Result"] = temp
+          
+        return result
+
+#####################################################################################ACCESSOR FUNCTIONS ##############################################
+
     def get_ext(self)->str:
         #accessor, return the extension of the file
         return self.__ext__
@@ -190,90 +302,15 @@ class prophet_table():
     def get_path(self)->str:
         #accessor, return the path of the file
         return self.__path__
-    
-    def set_show_leading(self)->None:
-        self.__show_leading__ = not(self.__show_leading__)
-    
-    def get_show_leading(self)->bool:
-        return self.__show_leading__
 
     def get_key_num(self)->int:
         #accessor, return the num of column used as key
         return self.__key_num__
+    def get_content(self)->pd.DataFrame:
+        return self.__content__
+
+#####################################################################################Pending  FUNCTIONS ##############################################
     
-    def compare(self, fac2)->pd.DataFrame:
-        # fac2 = table 2 you imported prophet_table expected
-        #  return the result of comparison with two columns [Lookup_Key,Result ]
-
-        if not(isinstance(fac2,prophet_table)):
-            raise Exception("The file is not prophet table object")
-        self.__gen_key__(True)
-        
-        fac2.__gen_key__(True)
-        
-        result=pd.DataFrame()   
-
-        try:
-            result["Lookup_Key"] = pd.concat([self.__content__["Key"], fac2["Key"]], axis=0, ignore_index=True).unique()
-        
-        except ValueError:
-            result["Lookup_Key"] = pd.Series(pd.concat([self.__content__["Key"],fac2["Key"]], axis=0, ignore_index=True).unique()).reindex()
-        
-        temp = []
-
-        for key in result["Lookup_Key"]:
-
-            check1 = self.__key_loc__(key,self.__content__["Key"])
-            check2 = self.__key_loc__(key, fac2["Key"])
-            
-            if check1!=-1 and check2!=-1 :
-                temp.append("Matched in both file")
-            
-            elif check1!=-1:
-                temp.append("Matched in fac1 only")
-            
-            else:
-                temp.append("Matched in fac2 only")
-        result["Result"] = temp
-        print("RESULT = \n{}".format(result))
-        return result
-  
-    def show_detail(self):
-        print(">>>>>>File source:\t {}".format(self.__path__+self.__file_name__+self.__ext__))
-        print(">>>>>>File type:\t {}".format((lambda x: (lambda y: "Model Point File" if y == ".PRO" else "Result file")(x) if x !=".fac" else "Table")(self.__ext__)))
-        print(">>>>>>File Shape:\t ({},{})".format(self.__content__.shape[0],(lambda x: x if self.__show_leading__ else x-1)(self.__content__.shape[1])))
-        print(">>>>>>File Settings:\t key_num = {},\t show_leading = {}".format(self.__key_num__,self.__show_leading__))
-        self.show_content()
-
-    def show_content(self)->None:
-        #printing function, print the table content
-        if self.__show_leading__:
-            print(self.__content__)
-        else:
-            print(self.__content__[[x for x in self.__content__.keys()[1:]]])
-  
-    def sort_records(self, target, target_col="", sort_timing="", inplace =False):
-        #path input the directory of the file, ending with \\ string type expected
-        #sort_timing, it used to sort the table with naming at specific timing string type expected
-        # target Required Value for sorting, string type expected
-        # target_col String, Traget location
-        #inplace Bool, True if you want to modified the table content inside the table object
-        #return the sorted result as new table
-        if self.__ext__!=".PRO":
-            raise Exception("Table Error, Not Model Point File")
-
-        if target == "" or target_col == "":
-            raise Exception("Policy No Criteria in string form is expected")
-        
-        if self.__file_name__.find(sort_timing) == -1:
-            raise Exception("This file does not on time point = {}".format(sort_timing))       
-
-        if inplace:
-            self.__content__ = self.__content__[self.__content__[target_col] == target]
-            return self
-        
-        return prophet_table(object = self.__content__[self.__content__[target_col] == target],path =self.get_path(),file = self.get_path()+self.get_ext(),show_leading= self.get_show_leading())
-
     def to_file(self, path="", as_csv=False)->None:
         #Path: String Expected Output path of the file
         #as_csv : Bool, True if you want to export as csv
@@ -282,16 +319,17 @@ class prophet_table():
         if  not(os.path.exists(path)):
             raise Exception("The Path is not exists")
         
-        print("Exporting {}".format(self.__file_name__+self.__ext__))
         path =  (lambda x : x+"\\" if x.rfind("\\") != len(x)-1 else x)(path)
+
         if as_csv:
+            print(">>>>>>Exporting to {} as {}.csv".format(path,self.__file_name__))
             if path!="":
                 self.__content__.to_csv( path+self.__file_name__+".csv")
             else:
                 self.__content__.to_csv(self.__path__+self.__file_name__+".csv")
                 
         else:
-            
+            print(">>>>>>Exporting to {} as {}".format(path,self.__file_name__+self.__ext__))
             pass
 
 
